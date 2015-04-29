f13d9a79035ea8d8753dce3a8428575e3e5ec1ff
jntakpe <j.ntakpe@gmail.com>
2015-04-29 22:02:23 +0200

    Spring initializer

---
+++ src/main/java/com/github/jntakpe/ezquiz/EzquizApplication.java
@@ -0,0 +1,12 @@
+package com.github.jntakpe.ezquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzquizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzquizApplication.class, args);
+    }
+}
