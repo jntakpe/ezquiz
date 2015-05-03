bd083699ca9ac7ce609bf7af96054ef2cdb78b64
jntakpe <j.ntakpe@gmail.com>
2015-05-03 22:46:06 +0200

    Reagencing

--- src/main/webapp/app/components/layout/layout.js
+++ src/main/webapp/app/components/layout/layout.js
@@ -1,27 +1,27 @@
 'use strict';
 
-import SidenavCtrl from './sidenav.controller.js';
-import HeaderCtrl from './header.controller.js';
+import SidenavCtrl from './sidenav/sidenav.controller';
+import HeaderCtrl from './header/header.controller';
 
 export default angular
-  .module('ezquiz-layout', ['ezquiz-core'])
-  .config($stateProvider => {
-    $stateProvider.state('layout', {
-      templateUrl: 'app/components/layout/layout.html'
-    }).state('main', {
-      parent: 'layout',
-      abstract: true,
-      views: {
-        'sidenav': {
-          templateUrl: 'app/components/layout/sidenav.html',
-          controller: SidenavCtrl,
-          controllerAs: 'sidenav'
-        },
-        'header': {
-          templateUrl: 'app/components/layout/header.html',
-          controller: HeaderCtrl,
-          controllerAs: 'header'
-        }
-      }
+    .module('ezquiz-layout', ['ezquiz-core'])
+    .config($stateProvider => {
+        $stateProvider.state('layout', {
+            templateUrl: 'app/components/layout/layout.html'
+        }).state('main', {
+            parent: 'layout',
+            abstract: true,
+            views: {
+                'sidenav': {
+                    templateUrl: 'app/components/layout/sidenav/sidenav.html',
+                    controller: SidenavCtrl,
+                    controllerAs: 'sidenav'
+                },
+                'header': {
+                    templateUrl: 'app/components/layout/header/header.html',
+                    controller: HeaderCtrl,
+                    controllerAs: 'header'
+                }
+            }
+        });
     });
-  });
